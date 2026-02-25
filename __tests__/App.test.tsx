6a9de02ab7e7bab0adccb547dae032c815f7d1b3
Chandan Kumar Mandal <chankruze@gmail.com>
2026-02-26 01:55:36 +0530

    HYFN-8: Fixed deprecated act usage by using async callback

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,20 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import App from '../src/app';
+
+jest.mock('@tanstack/react-query-persist-client', () => ({
+  PersistQueryClientProvider: ({ children }: { children: React.ReactNode }) =>
+    children,
+}));
+
+jest.mock('@/lib/query-client', () => ({
+  queryClient: {},
+  persister: {},
+}));
 
 test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
+  await ReactTestRenderer.act(async () => {
     ReactTestRenderer.create(<App />);
   });
 });
